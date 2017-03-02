c783accbca1cb0d49e5473b5cea2ca2ac199d11e
jimyandres <jimyandres@utp.edu.co>
2017-03-01 22:30:13 -0500

    Problem with saving/reading binary file

--- hash.cc
+++ hash.cc
@@ -4,6 +4,8 @@
 #include <sstream>
 #include <iomanip>
 #include <iostream>
+#include <fstream>
+
 #include "uthash.h"
 
 
@@ -12,8 +14,8 @@
 using namespace std;
 
 struct files_hashmap {
-    char checksum[40];        	/* key (string is WITHIN the structure) */
-    char server_address[16];
+    char checksum[SHA_DIGEST_LENGTH*2+1];        	/* key (string is WITHIN the structure) */
+    string server_address;
     UT_hash_handle hh;			/* makes this structure hashable */
 };
 
@@ -22,30 +24,38 @@ string GetHexRepresentation(const unsigned char * Bytes)
     ostringstream os;
     os.fill('0');
     os<<hex;
-    for(const unsigned char * ptr=Bytes;ptr<Bytes+20;ptr++)
+    for(const unsigned char * ptr=Bytes;ptr<Bytes+SHA_DIGEST_LENGTH;ptr++)
         os<<setw(2)<<(unsigned int)*ptr;
 
-    //cout << os.str();
     return os.str();
 }
 
-void writeDisk(files_hashmap *s)
+void writeDisk(files_hashmap s)
 {
-    FILE *file = fopen("files.dat", "wb");
+    ofstream output_file("files.dat", ios::binary);
+    output_file.write((char*)&s, sizeof(s));
+    output_file.close();
+
+
+    /*FILE *file = fopen("files.dat", "wb");
 
     if (!file) {
 		printf("Unable to open file!");
 		return;
 	}
+    fseek(file, 0, SEEK_END);
 
     fwrite(&s, sizeof(s), 1, file);
 
-    fclose(file);
+    fclose(file);*/
 }
 
-void loadDisk(files_hashmap *s)
+void loadDisk(files_hashmap s)
 {
-    //char *buffer;
+    ifstream input_file("files.dat", ios::binary);
+    input_file.read((char*)&s, sizeof(s)); 
+
+    /*//char *buffer;
     long lSize;
     FILE *file = fopen("files.dat", "rb");
 
@@ -61,7 +71,7 @@ void loadDisk(files_hashmap *s)
     //s = (struct files_hashmap*)malloc(sizeof(struct files_hashmap));
 
     fread(s, 1, sizeof(s), file);
-    fclose(file);       
+    fclose(file); */      
 }
 
 int main(int argc, char *argv[]) {
@@ -95,29 +105,65 @@ int main(int argc, char *argv[]) {
 
     cks = GetHexRepresentation(check_sum);
 
-    cout << cks << endl;
+    cout << cks.c_str() << endl;
 
-    s = (struct files_hashmap*)malloc(sizeof(struct files_hashmap));
+
+    //s = (struct files_hashmap*)malloc(sizeof(struct files_hashmap));
+    s = new struct files_hashmap();
     
-    strncpy(s->checksum, cks.c_str(), cks.length());
+    strncpy(s->checksum, cks.c_str(), SHA_DIGEST_LENGTH*2);
     string tt=  "123.123.123.123";
-    strncpy(s->server_address, tt.c_str(), 40);
-    //s->server_address = "123.123.123.123";
+    s->server_address = tt;
+    //HASH_ADD_STR( files, checksum, s );
+    cout << s->checksum << "\naaaa" << endl << s->server_address.c_str() << endl;
+    
+    //writeDisk(*s);
+
+    /*ofstream output_file("files.dat", ios::binary);
+    output_file.write((char*)s, sizeof(*s));
+    //output_file.write(reinterpret_cast<char*>(s), sizeof(s));
+    output_file.close();*/
+
+    s = new struct files_hashmap();
+
+    string t2 = "abcdefghijklmnopqrstuvwxyz12345678910124";
+    
+    strncpy(s->checksum, t2.c_str(), SHA_DIGEST_LENGTH*2);
+    string t3=  "123.123.123.1555";
+    //strncpy(s->server_address, tt.c_str(), 40);
+    s->server_address = t3;
     HASH_ADD_STR( files, checksum, s );
-    cout << s->checksum << "aaaa" << endl << s->server_address;
-    cout << "\naadsad\n"<<s<<" " <<sizeof(s);
+    cout << s->checksum << endl << s->server_address.c_str() << endl;
 
-    cks += tt;
+    ofstream output_file("files.dat", ios::binary);
+    output_file.write((char*)s, sizeof(*s));
+    //output_file.write(reinterpret_cast<char*>(s), sizeof(s));
+    output_file.close();
+
+
+    //cks += tt;
     HASH_FIND_STR( files, cks.c_str(), s);
-    if (s) printf("a.mkv's id is %s\n", s->server_address);
+    if (s) printf("a.mkv's id is %s\n", s->server_address.c_str());
+    else cout << "\nNot found\n";
 
-    writeDisk(s);
-    temp = (struct files_hashmap*)malloc(sizeof(struct files_hashmap));
-    loadDisk(temp);
+    //writeDisk(s);
+    temp = (struct files_hashmap*)malloc(136);
+    //temp = new struct files_hashmap();
+    //loadDisk(*temp);
+
+    ifstream input_file("files.dat", ios::binary);
+    //input_file.read((char*)&temp, sizeof(temp)); 
+    input_file.read(reinterpret_cast<char*>(temp), sizeof(temp));
+    cout << "\nsize " << sizeof(*temp);
+
+
+    t2 = "abcdefghijklmnop";
+
+    //temp = (struct files_hashmap*)temp;
 
     HASH_FIND_STR( files, cks.c_str(), temp);
-    if (temp) printf("check_sum's location is %s\n", temp->server_address);
-    else cout << "\nNot found";
+    if (temp) printf("check_sum's location is %s\n", temp->server_address.c_str());
+    else cout << "\nNot found\n";
     
     HASH_ITER(hh, files, temp, tmp) {
         HASH_DEL(files, temp);
@@ -131,65 +177,3 @@ int main(int argc, char *argv[]) {
     }
     return 0;
 }
-
-/*int main(int argc, char *argv[]) {
-
-	//unsigned char *cs[] = reinterpret_cast<const unsigned char *>( { "joe", "bob", "betty", NULL } );
-
-
-	size_t size, total, size_sha1;
-	char * data, *data_sha1 = NULL;
-	string check_sum[SHA_DIGEST_LENGTH];
-	long sz;
-	FILE * f;
-
-	if(!(f = fopen("uthash", "rb"))) {
-		return 0;
-	}
-
-	fflush(f);
-	fseek(f, 0L, SEEK_END);
-	sz = ftell(f);
-	fseek(f, 0L, SEEK_SET);
-
-	data_sha1 = (char*) malloc (sizeof(char)*sz);
-	size_sha1 = fread(data_sha1, 1, sz, f);
-
-	SHA1((unsigned char *)data_sha1, size_sha1, (string *)&check_sum);
-	free(data_sha1);
-
-    const char **n; //, *names[] = { "joe", "bob", "betty", NULL };
-    struct files_hashmap *s, *tmp, *files = NULL, *temp;
-    int i=0;
-
-    //for (n = cs; *n != NULL; n++) {
-        s = (struct files_hashmap*)malloc(sizeof(struct files_hashmap));
-        strncpy((const char*)s->checksum, check_sum, sizeof(check_sum));
-        s->server_address = "123.123.123.123";
-        //HASH_ADD_STR( files, checksum, s );
-        HASH_ADD_KEYPTR( hh, files, s->checksum, strlen((const char*)s->checksum), s );
-    //}
-
-    HASH_FIND_STR( files, check_sum, s);
-    if (s) printf("check_sum's location is %s\n", s->server_address);
-
-    writeDisk(s);
-    temp = (struct files_hashmap*)malloc(sizeof(struct files_hashmap));
-    loadDisk(temp);
-
-    HASH_FIND_STR( files, "betty", temp);
-    if (temp) printf("check_sum's location is %s\n", temp->server_address);
-
-
-    
-    HASH_ITER(hh, files, s, tmp) {
-		HASH_DEL(files, s);
-		free(s);
-    }
-
-    HASH_ITER(hh, files, temp, tmp) {
-		HASH_DEL(files, temp);
-		free(temp);
-    }
-    return 0;
-}*/
